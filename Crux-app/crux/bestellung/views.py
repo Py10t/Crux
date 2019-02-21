c75efe56c2513b1e0a64c9e373dc0c5801245616
Py10t <a-economic@gmx.de>
2019-02-21 01:41:17 +0100

    21.02

--- Crux-app/crux/bestellung/views.py
+++ Crux-app/crux/bestellung/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+import datetime
+
 # Create your views here.
 
 def index(request):
 
+    dt = datetime.datetime.now()
+    aktuelle_kw = dt.isocalendar()[1]
+    aktuelle_kw_string = "KW " + str(aktuelle_kw)
+
     template = loader.get_template('bestellung/index.html')
     context = {
-
+        'aktuelle_kw_string': aktuelle_kw_string
     }
+
+
+
     return HttpResponse(template.render(context, request))
