17bf26972ee30120c120d1d73050f6a1be1b9e32
Py10t <a-economic@gmx.de>
2019-03-27 01:46:36 +0100

    27.03 progress was made

--- Crux-app/crux/bestellung/views.py
+++ Crux-app/crux/bestellung/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django_tables2 import RequestConfig
 from .models import Order
 from .tables import OrderTable
+from django.shortcuts import redirect
+from .forms import OrderForm
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 # gets table data and displays it
@@ -19,6 +23,46 @@ def update_order(request, record_id):
     obj.order_status = 'Auftrag'
     obj.save()
     print('item updated: ' + str(record_id))
-    table = OrderTable(Order.objects.filter(order_status="Bestellung"))
-    RequestConfig(request).configure(table)
-    return render(request, 'bestellung/index.html', {'table': table})
+    response = redirect('/bestellung/')
+    return response
+    # table = OrderTable(Order.objects.filter(order_status="Bestellung"))
+    # RequestConfig(request).configure(table)
+    # return render(request, 'bestellung/index.html', {'table': table})
+
+# def create_album(request):
+#     if not request.user.is_authenticated():
+#         return render(request, 'music/login.html')
+#     else:
+#         form = OrderForm(request.POST or None, request.FILES or None)
+#         if form.is_valid():
+#             album = form.save(commit=False)
+#             album.user = request.user
+#             album.album_logo = request.FILES['album_logo']
+#             file_type = album.album_logo.url.split('.')[-1]
+#             file_type = file_type.lower()
+#             if file_type not in IMAGE_FILE_TYPES:
+#                 context = {
+#                     'album': album,
+#                     'form': form,
+#                     'error_message': 'Image file must be PNG, JPG, or JPEG',
+#                 }
+#                 return render(request, 'music/create_album.html', context)
+#             album.save()
+#             return render(request, 'music/detail.html', {'album': album})
+#         context = {
+#             "form": form,
+#         }
+#         return render(request, 'music/create_album.html', context)
+
+def create_order(request):
+    form = OrderForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        order = form.save(commit=False)
+        order.save()
+        response = redirect('/bestellung/')
+        return response
+
+    context = {
+        "form": form,
+    }
+    return render(request, 'bestellung/order_form.html', context)
--- Crux-app/crux/stock/models.py
+++ Crux-app/crux/stock/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Material(models.Model):
-    name = models.CharField(max_length=250)
-    chem_name = models.CharField(max_length=250)
-    company =  models.CharField(max_length=250)
+    name = models.CharField(("Material"), max_length=250)
+    chem_name = models.CharField(("Tech. Name"),max_length=250)
+    company =  models.CharField(("Firma"), max_length=250)
 
     def __str__(self):
         return self.name
@@ -13,14 +13,14 @@ class Material(models.Model):
 
 
 class Article(models.Model):
-    ext_number = models.CharField(max_length=250)
-    int_number = models.CharField(max_length=250)
-    name = models.CharField(max_length=250)
+    ext_number = models.CharField(("Ext. ID"), max_length=250)
+    int_number = models.CharField(("Int. ID"), max_length=250)
+    name = models.CharField(("Artikel"), max_length=250)
     material = models.ForeignKey(Material, on_delete=models.CASCADE)
-    weight = models.DecimalField(max_digits=8, decimal_places=3)
-    stock_amount = models.BigIntegerField()
-    currently_produced = models.BigIntegerField()
-    price_per_piece = models.DecimalField(max_digits=8, decimal_places=2)
+    weight = models.DecimalField(("Gewicht"), max_digits=8, decimal_places=3)
+    stock_amount = models.BigIntegerField(("Lagerbestand"), )
+    currently_produced = models.BigIntegerField(("In Produktion"), )
+    price_per_piece = models.DecimalField(("Preis in €"), max_digits=8, decimal_places=2)
 
     def __str__(self):
         return self.name + ' | ' + str(self.material)
